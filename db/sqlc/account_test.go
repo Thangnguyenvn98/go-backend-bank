194aaf1094adabcf44a6a53378cc476d85fafb2a
thangnguyenvn98 <thangnguyenvn647@gmail.com>
2025-01-23 19:51:53 -0500

    Adding test for get account and creating a helper function to create an account

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -3,12 +3,13 @@ package db
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 	"github.com/thangnguyenvn98/simplebank/util"
 )
 
-func TestCreateAccount(t *testing.T) {
+func CreateRandomAccount(t *testing.T) Account {
 	arg := CreateAccountParams{Owner: util.RandomOwner(), Balance: util.RandomMoney(), Currency: util.RandomCurrency()}
 
 	account, err := testQueries.CreateAccount(context.Background(), arg)
@@ -22,8 +23,24 @@ func TestCreateAccount(t *testing.T) {
 	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
 
+	return account
+}
+
+func TestCreateAccount(t *testing.T) {
+	CreateRandomAccount(t)
+
 }
 
 func TestGetAccount(t *testing.T) {
 	// Create Account first so it is independent
+	account1 := CreateRandomAccount(t)
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, account1.Balance, account2.Balance)
+	require.Equal(t, account1.Currency, account2.Currency)
+	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
+
 }
