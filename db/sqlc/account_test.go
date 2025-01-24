17cc88156559615bf0ae52547ef5d7db3c112d49
thangnguyenvn98 <thangnguyenvn647@gmail.com>
2025-01-23 19:46:19 -0500

    Adding first set of test, with util for creating random number

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -5,10 +5,11 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"github.com/thangnguyenvn98/simplebank/util"
 )
 
 func TestCreateAccount(t *testing.T) {
-	arg := CreateAccountParams{Owner: "tom", Balance: 100, Currency: "USD"}
+	arg := CreateAccountParams{Owner: util.RandomOwner(), Balance: util.RandomMoney(), Currency: util.RandomCurrency()}
 
 	account, err := testQueries.CreateAccount(context.Background(), arg)
 	require.NoError(t, err)
@@ -18,4 +19,11 @@ func TestCreateAccount(t *testing.T) {
 	require.Equal(t, arg.Balance, account.Balance)
 	require.Equal(t, arg.Currency, account.Currency)
 
+	require.NotZero(t, account.ID)
+	require.NotZero(t, account.CreatedAt)
+
+}
+
+func TestGetAccount(t *testing.T) {
+	// Create Account first so it is independent
 }
